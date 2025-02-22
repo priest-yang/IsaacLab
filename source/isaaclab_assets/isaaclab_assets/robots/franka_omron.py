4a7e233bfb8e9b41f29d3885b167a505650e31f6
priest-yang <shaozey@umich.edu>
2025-02-21 21:51:36 -0500

    init commit

---
+++ source/isaaclab_assets/isaaclab_assets/robots/franka_omron.py
@@ -0,0 +1,123 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Configuration for the Franka Emika robots.
+
+The following configurations are available:
+
+* :obj:`FRANKA_OMRON_CFG`: Franka Emika Panda robot with Panda hand
+* :obj:`FRANKA_OMRON_HIGH_PD_CFG`: Franka Emika Panda robot with Panda hand with stiffer PD control
+
+Reference: https://github.com/frankaemika/franka_ros
+"""
+
+import isaaclab.sim as sim_utils
+from isaaclab.actuators import ImplicitActuatorCfg
+from isaaclab.assets.articulation import ArticulationCfg
+from isaaclab.utils.assets import ISAACLAB_NUCLEUS_DIR
+
+##
+# Configuration
+##
+
+FRANKA_OMRON_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=f'/home/johndoe/Documents/IsaacLab/source/isaaclab_assets/data/omron_franka.usd',
+        activate_contact_sensors=False,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            max_depenetration_velocity=5.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=True, solver_position_iteration_count=8, solver_velocity_iteration_count=0
+        ),
+        # collision_props=sim_utils.CollisionPropertiesCfg(contact_offset=0.005, rest_offset=0.0),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        joint_pos={
+            "panda_joint1": 0.0,
+            "panda_joint2": -0.569,
+            "panda_joint3": 0.0,
+            "panda_joint4": -2.810,
+            "panda_joint5": 0.0,
+            "panda_joint6": 3.037,
+            "panda_joint7": 0.741,
+            "panda_finger_joint.*": 0.04,
+
+            # base
+            "mobilebase_forward": 0.0,
+            "mobilebase_side": 0.0,
+            "mobilebase_yaw": 0.0,
+            "mobilebase_torso_height": 0.0,
+        },
+    ),
+    actuators={
+        "panda_shoulder": ImplicitActuatorCfg(
+            joint_names_expr=["panda_joint[1-4]"],
+            effort_limit=87.0,
+            velocity_limit=2.175,
+            stiffness=80.0,
+            damping=4.0,
+        ),
+        "panda_forearm": ImplicitActuatorCfg(
+            joint_names_expr=["panda_joint[5-7]"],
+            effort_limit=12.0,
+            velocity_limit=2.61,
+            stiffness=80.0,
+            damping=4.0,
+        ),
+        "panda_hand": ImplicitActuatorCfg(
+            joint_names_expr=["panda_finger_joint.*"],
+            effort_limit=200.0,
+            velocity_limit=0.2,
+            stiffness=2e3,
+            damping=1e2,
+        ),
+        "mobile_base_movement": ImplicitActuatorCfg(
+            joint_names_expr=["mobilebase_forward", "mobilebase_side", ],
+            effort_limit=100000,
+            velocity_limit=1,
+            stiffness=1e6,
+            damping=1e4,
+        ),
+        "mobile_base_rotate": ImplicitActuatorCfg(
+            joint_names_expr=["mobilebase_yaw"],
+            effort_limit=100000,
+            velocity_limit=5,
+            stiffness=1e6,
+            damping=1e4,
+        ),
+        "mobile_base_torso": ImplicitActuatorCfg(
+            joint_names_expr=["mobilebase_torso_height"],
+            effort_limit=100000,
+            velocity_limit=0.5,
+            stiffness=1e6,
+            damping=1e4,
+        ),
+
+    },
+    soft_joint_pos_limit_factor=1.0,
+)
+"""Configuration of Franka Emika Panda robot."""
+
+
+FRANKA_OMRON_HIGH_PD_CFG = FRANKA_OMRON_CFG.copy()
+FRANKA_OMRON_HIGH_PD_CFG.spawn.rigid_props.disable_gravity = True
+FRANKA_OMRON_HIGH_PD_CFG.actuators["panda_shoulder"].stiffness = 400.0
+FRANKA_OMRON_HIGH_PD_CFG.actuators["panda_shoulder"].damping = 80.0
+FRANKA_OMRON_HIGH_PD_CFG.actuators["panda_forearm"].stiffness = 400.0
+FRANKA_OMRON_HIGH_PD_CFG.actuators["panda_forearm"].damping = 80.0
+
+FRANKA_OMRON_HIGH_PD_CFG.actuators["mobile_base_movement"].stiffness = 1e6
+FRANKA_OMRON_HIGH_PD_CFG.actuators["mobile_base_movement"].damping = 1e4
+FRANKA_OMRON_HIGH_PD_CFG.actuators["mobile_base_torso"].stiffness = 1e6
+FRANKA_OMRON_HIGH_PD_CFG.actuators["mobile_base_torso"].damping = 1e4
+FRANKA_OMRON_HIGH_PD_CFG.actuators["mobile_base_rotate"].stiffness = 1e6
+FRANKA_OMRON_HIGH_PD_CFG.actuators["mobile_base_rotate"].damping = 1e4
+
+"""Configuration of Franka Emika Panda robot with stiffer PD control.
+
+This configuration is useful for task-space control using differential IK.
+"""
---
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/lift/config/franka_omron/joint_pos_env_cfg.py
@@ -0,0 +1,99 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from isaaclab.assets import RigidObjectCfg
+from isaaclab.sensors import FrameTransformerCfg
+from isaaclab.sensors.frame_transformer.frame_transformer_cfg import OffsetCfg
+from isaaclab.sim.schemas.schemas_cfg import RigidBodyPropertiesCfg
+from isaaclab.sim.spawners.from_files.from_files_cfg import UsdFileCfg
+from isaaclab.utils import configclass
+from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
+
+from isaaclab_tasks.manager_based.manipulation.lift import mdp
+from isaaclab_tasks.manager_based.manipulation.lift.lift_env_cfg import LiftEnvCfg
+
+##
+# Pre-defined configs
+##
+from isaaclab.markers.config import FRAME_MARKER_CFG  # isort: skip
+from isaaclab_assets.robots.franka_omron import FRANKA_OMRON_CFG  # isort: skip
+
+
+@configclass
+class FrankaOmronCubeLiftEnvCfg(LiftEnvCfg):
+    def __post_init__(self):
+        # post init of parents
+        super().__post_init__()
+
+        # Set Franka as robot
+        self.scene.robot = FRANKA_OMRON_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
+
+        # Set actions for the specific robot type (franka)
+        self.actions.arm_action = mdp.JointPositionActionCfg(
+            asset_name="robot", joint_names=["panda_joint.*"], scale=0.5, use_default_offset=True
+        )
+        self.actions.gripper_action = mdp.BinaryJointPositionActionCfg(
+            asset_name="robot",
+            joint_names=["panda_finger.*"],
+            open_command_expr={"panda_finger_.*": 0.04},
+            close_command_expr={"panda_finger_.*": 0.0},
+        )
+        self.actions.base_action = mdp.RelativeJointPositionActionCfg(
+            asset_name="robot",
+            joint_names=["mobilebase_.*"],
+            scale=0.2,
+            use_zero_offset=True, # use default offset is not working for base action
+        )
+        # Set the body name for the end effector
+        self.commands.object_pose.body_name = "panda_hand"
+
+        # Set Cube as object
+        self.scene.object = RigidObjectCfg(
+            prim_path="{ENV_REGEX_NS}/Object",
+            init_state=RigidObjectCfg.InitialStateCfg(pos=[0.5, 0, 0.055], rot=[1, 0, 0, 0]),
+            spawn=UsdFileCfg(
+                usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/Blocks/DexCube/dex_cube_instanceable.usd",
+                scale=(0.8, 0.8, 0.8),
+                rigid_props=RigidBodyPropertiesCfg(
+                    solver_position_iteration_count=16,
+                    solver_velocity_iteration_count=1,
+                    max_angular_velocity=1000.0,
+                    max_linear_velocity=1000.0,
+                    max_depenetration_velocity=5.0,
+                    disable_gravity=False,
+                ),
+            ),
+        )
+
+        # Listens to the required transforms
+        marker_cfg = FRAME_MARKER_CFG.copy()
+        marker_cfg.markers["frame"].scale = (0.1, 0.1, 0.1)
+        marker_cfg.prim_path = "/Visuals/FrameTransformer"
+        self.scene.ee_frame = FrameTransformerCfg(
+            prim_path="{ENV_REGEX_NS}/Robot/omron_v2/world",
+            debug_vis=True,
+            visualizer_cfg=marker_cfg,
+            target_frames=[
+                FrameTransformerCfg.FrameCfg(
+                    prim_path="{ENV_REGEX_NS}/Robot/omron_v2/Franka/panda_hand",
+                    name="end_effector",
+                    offset=OffsetCfg(
+                        pos=[0.0, 0.0, 0.1034],
+                    ),
+                ),
+            ],
+        )
+
+
+@configclass
+class FrankaOmronCubeLiftEnvCfg_PLAY(FrankaOmronCubeLiftEnvCfg):
+    def __post_init__(self):
+        # post init of parent
+        super().__post_init__()
+        # make a smaller scene for play
+        self.scene.num_envs = 50
+        self.scene.env_spacing = 2.5
+        # disable randomization for play
+        self.observations.policy.enable_corruption = False
